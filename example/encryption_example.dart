7dbab7e67f183f24aeca142a23ce11224c082bec
AbdAlftahSalem <abd321alftah@gmail.com>
2024-08-09 12:35:21 +0300

    Export folders and files ...

--- example/encryption_example.dart
+++ example/encryption_example.dart
@@ -1,6 +1,10 @@
-// import 'package:encryption/encryption.dart';
-//
-// void main() {
-//   var awesome = Awesome();
-//   print('awesome: ${awesome.isAwesome}');
-// }
+import '../lib/encryption.dart';
+
+void main() {
+  String plainText = "abd alftah salem";
+  String cipherText = TextEncryption.textEncryption(plainText);
+  String encipherText = TextDecryption.textDecryption(cipherText);
+  print('Plain Text    : $plainText');
+  print('Cipher Text   : $cipherText');
+  print('Encipher Text : $encipherText');
+}
--- lib/encryption.dart
+++ lib/encryption.dart
@@ -3,4 +3,13 @@
 /// More dartdocs go here.
 library;
 
+export 'core/en_letters.dart';
+export 'core/model/key_model.dart';
+export 'core/model/letters_key_model.dart';
+export 'feature/key_generation/extract_key.dart';
+export 'feature/key_generation/generate_key.dart';
+export 'feature/text_encryption_decryption/text_decryption.dart';
+export 'feature/text_encryption_decryption/text_encryption.dart';
+export 'main.dart';
+
 // TODO: Export any libraries intended for clients of this package.
