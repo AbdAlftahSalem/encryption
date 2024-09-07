464390eb366e2b3cde4fc339b5eb014a9fbf4c46
AbdAlftahSalem <abd321alftah@gmail.com>
2024-09-07 17:44:07 +0300

    Hide length words in chipher text

--- lib/feature/text_encryption_decryption/text_encryption.dart
+++ lib/feature/text_encryption_decryption/text_encryption.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import '../../core/model/key_model.dart';
 import '../../core/model/letters_key_model.dart';
 import '../../core/strings/letters_init.dart';
@@ -35,6 +37,9 @@ class TextEncryption {
       encryptedString,
       keyModel.letters,
     );
+
+    String encryptedStringWithKeyWithLength =
+        _addLengthToCipheredText(encryptedStringWithKey);
 
     return encryptedStringWithKey;
   }
@@ -97,5 +102,34 @@ class TextEncryption {
           lettersKeyModel.l6;
     }
     return newCipherText;
+  }
+
+  static String _addLengthToCipheredText(String encryptedStringWithKey) {
+    LettersInit.instance.length;
+    List<String> lettersLength = [];
+
+    bool searchFlag = true;
+
+    while (searchFlag) {
+      int firstNum = Random().nextInt(LettersInit.instance.length);
+      int secNum = Random().nextInt(LettersInit.instance.length);
+
+      String firstLetter =
+          LettersInit.instance.lettersWithIntKey[firstNum] ?? "";
+      String secLetter = LettersInit.instance.lettersWithIntKey[secNum] ?? "";
+
+      int firstInt =
+          LettersInit.instance.lettersWithStringKey[firstLetter] ?? 0;
+      int secInt = LettersInit.instance.lettersWithStringKey[secLetter] ?? 0;
+
+      if (firstInt + secInt == LettersInit.instance.length) {
+        lettersLength.add(firstLetter);
+        lettersLength.add(secLetter);
+        searchFlag = false;
+      }
+    }
+    encryptedStringWithKey =
+        lettersLength[0] + encryptedStringWithKey + lettersLength[1];
+    return encryptedStringWithKey;
   }
 }
